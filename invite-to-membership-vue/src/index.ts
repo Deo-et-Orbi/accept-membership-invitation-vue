6a04e2b384194427a5419e058c7f86694fb81155
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-03-26 20:32:52 +0100

    feat: prepare lib release

---
+++ invite-to-membership-vue/src/index.ts
@@ -0,0 +1,40 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.config.productionTip = false;
+
+export async function mountInviteToMembershipVue(
+  mountTarget: string
+): Promise<void> {
+  async function initFirebaseFromJSON(url: string) {
+    const response = await fetch(url);
+    firebase.initializeApp(await response.json());
+  }
+  function initVue() {
+    new Vue({
+      render: (h) => h(App),
+    }).$mount(mountTarget);
+  }
+
+  try {
+    await initFirebaseFromJSON("/__/firebase/init.json");
+  } catch (err) {
+    console.warn(
+      "Failed to load firebase from firebase hosting, " +
+        "trying to load dev config. Error: ",
+      err
+    );
+    await initFirebaseFromJSON("/firebase.init.dev.json");
+    console.log("Successfully loaded firebase dev config");
+  }
+  initVue();
+}
