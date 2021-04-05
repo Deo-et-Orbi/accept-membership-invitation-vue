c41b8131ab3568e579076a68aa7594e8e70be0a2
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-04-05 23:36:27 +0200

    fix: update api paths

--- app/src/services.ts
+++ app/src/services.ts
@@ -2,11 +2,11 @@ import { Invitation } from "./domain";
 import { fetchFromPublicApi, postToApi } from "./util";
 
 export function loadInvitation(id: string): Promise<Invitation> {
-  return fetchFromPublicApi(`public_api/invitation/${id}`);
+  return fetchFromPublicApi(`invitation/get/${id}`);
 }
 
 export function acceptMembership(invitationID: string): Promise<Invitation> {
-  return postToApi(`nonmember_api/accept_invitation`, {
+  return postToApi(`invitation/accept`, {
     invitationID,
   });
 }
